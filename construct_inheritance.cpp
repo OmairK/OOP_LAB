e9b8f4ed14c37794821fbbafaf9377932b203cb5
Omair Khan <omairkhan064@gmail.com>
2019-10-03 19:43:02 +0530

    Initial commit 2

---
+++ construct_inheritance.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+
+using namespace std;
+
+class Hexa
+{
+private:
+    int Hn;
+
+public:
+    Hexa()
+    {
+        cout << "Hexa unparametrized constructor called" << endl;
+    }
+    Hexa(int n) : Hn(n)
+    {
+        cout << "Hexa parametrized constructor called" << endl;
+    }
+    ~Hexa()
+    {
+        cout<<"Hexa destructor called"<<endl;
+    }
+};
+
+class Alpha : public Hexa
+{
+private:
+    int Aa, Ab;
+    int c;
+
+public:
+    Alpha()
+    {
+        c = 100;
+        cout << "Alpha unparametrized constructor called" << endl;
+    }
+    Alpha(int arg1, int arg2, int arg3) : Aa(arg1), Ab(arg2), c(arg3)
+    {
+        cout << "Alpha parametrized constructor called" << endl;
+    }
+    int getC()
+    {
+        return c;
+    }
+    ~Alpha()
+    {
+        cout<<"Alpha destructor called"<<endl;
+    }
+};
+
+class Beta : public Hexa
+{
+private:
+    int Bd, Be;
+    int c;
+
+public:
+    Beta()
+    {
+        c = 10;
+        cout << "Beta unparametrized constructor called" << endl;
+    }
+    Beta(int arg1, int arg2, int arg3) : c(arg1), Bd(arg2), Be(arg3)
+    {
+        cout << "Beta parametrized constructor called" << endl;
+    }
+    int getC()
+    {
+        return c;
+    }
+    ~Beta()
+    {
+        cout<<"Beta destructor called"<<endl;
+    }
+};
+
+class Gamma : public Beta, public Alpha
+{
+private:
+    int f, g;
+
+public:
+    Gamma()
+    {
+        cout << "Gamma unparametrized constructor called" << endl;
+    }
+    Gamma(int arg1, int arg2) : f(arg1), g(arg2)
+    {
+        cout << "Gamma parametrized constructor called" << endl;
+    }
+    ~Gamma() 
+    {
+        cout << "Gamma destructor called" << endl;
+    }
+    // void display()
+};
+
+int main()
+{
+    Gamma x;
+    cout << x.Alpha::getC() << endl;
+
+    return 0;
+}
