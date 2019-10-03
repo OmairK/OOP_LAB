a5f27773ae8749ec14043bc906ff0891024d900b
Omair Khan <omairkhan064@gmail.com>
2019-10-03 19:48:52 +0530

    Commit 5

---
+++ friend.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+
+
+class Box{
+    
+        
+    public:
+        float length,breadth,height;
+        Box(){}
+        Box(float l,float b,float h):length(l),breadth(b),height(h){}
+        
+        friend double Volume(Box box);
+        friend double SurfaceArea(Box box);
+        inline void display();
+
+        
+
+};
+
+inline void Box::display(){
+            cout<<"\n Dimensions \n     Height: "<<height<<"\n    Length: "<<length<<"\n    Breadth: "<<breadth<<endl;
+        }
+
+double volume(Box box){
+    return(box.length*box.height*box.breadth);
+}
+
+double surfaceArea(Box box){
+    return(2*(box.length*box.height + box.breadth*box.height + box.length*box.breadth));
+}
+
+int main(){
+    float len,bre,hei;
+    cout<<"Enter the dimensions of the box"<<endl;
+    cout<<"Enter the length"<<endl;
+    cin>>len;
+    cout<<"Enter the breadth"<<endl;
+    cin>>bre;
+    cout<<"Enter the height"<<endl;
+    cin>>hei;
+
+    Box box = Box(len,bre,hei);
+    box.display();
+    cout<<"The surface area is "<<surfaceArea(box)<<endl;
+    cout<<"The volume is "<<volume(box)<<endl;
+    return 1;
+
+}
